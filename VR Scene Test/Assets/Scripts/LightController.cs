393785d4b26604cb8e8ec9d085834b6d0f5a3fee
Jack Smith <streamlined50@hotmail.com>
2018-10-15 18:44:37 +0100

    Adding Current Scene to Version Control

---
+++ VR Scene Test/Assets/Scripts/LightController.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour {
+
+	public enum LightConfiguration
+	{ONE, TWO, THREE, FOUR, FIVE, SIX}
+	public LightConfiguration lightconfig;
+	
+	// Load Light Properties
+	void Start () {
+		// Load Lights In
+		// Room 1
+		Light R1L1 = GameObject.Find ("R1L1").GetComponent<Light> ();
+		Light R1L2 = GameObject.Find ("R1L2").GetComponent<Light> ();
+		// Room 2
+		Light R2L1 = GameObject.Find ("R2L1").GetComponent<Light> ();
+		Light R2L2 = GameObject.Find ("R2L2").GetComponent<Light> ();
+		// Room 3
+		Light R3L1 = GameObject.Find ("R3L1").GetComponent<Light> ();
+		Light R3L2 = GameObject.Find ("R3L2").GetComponent<Light> ();
+		// Room 4
+		Light R4L1 = GameObject.Find ("R4L1").GetComponent<Light> ();
+		Light R4L2 = GameObject.Find ("R4L2").GetComponent<Light> ();
+		// Room 5
+		Light R5L1 = GameObject.Find ("R5L1").GetComponent<Light> ();
+		Light R5L2 = GameObject.Find ("R5L2").GetComponent<Light> ();
+		// Room 6
+		Light R6L1 = GameObject.Find ("R6L1").GetComponent<Light> ();
+		Light R6L2 = GameObject.Find ("R6L2").GetComponent<Light> ();
+		// Room 7
+		Light R7L1 = GameObject.Find ("R7L1").GetComponent<Light> ();
+		Light R7L2 = GameObject.Find ("R7L2").GetComponent<Light> ();
+
+		if (lightconfig == LightConfiguration.ONE) {
+			// Starting Room 1
+			// Light 1
+			R1L1.color = Color.blue; 
+			// Light 2
+			R1L2.color = Color.red; 
+
+			//Room 2
+			// Light 1
+			R2L1.intensity = 0.1f;
+			// Light 2
+			R2L2.intensity = 1.0f;
+
+			//Room 4
+			//Light 1
+			R4L1.intensity = 0.1f;
+			R4L1.color = Color.red;	
+			// Light 2
+			R4L2.intensity = 1.0f;
+			R4L2.color = Color.blue;
+
+			//Room 5
+			//Light 1
+			R5L1.intensity = 0.1f;
+			R5L1.color = Color.red;	
+			// Light 2
+			R5L2.intensity = 1.0f;
+			R5L2.color = Color.blue;
+
+			//Room 3
+			// Light 1
+			R3L1.intensity = 0.1f;
+			// Light 3
+			R3L2.intensity = 1.0f;
+
+			//Room 6
+			//Light 1
+			R6L1.intensity = 0.1f;
+			R6L1.color = Color.red;	
+			// Light 2
+			R6L2.intensity = 1.0f;
+			R6L2.color = Color.blue;
+
+			//Room 7
+			//Light 1
+			R7L1.intensity = 0.1f;
+			R7L1.color = Color.red;	
+			// Light 2
+			R7L2.intensity = 1.0f;
+			R7L2.color = Color.blue;
+
+		} else if (lightconfig == LightConfiguration.TWO) {
+			// Starting Room 1
+			// Light 1
+			R1L1.intensity = 0.1f;
+			// Light 2
+			R1L2.intensity = 1.0f;
+
+			//Room 2
+			// Light 1
+			R2L1.color = Color.blue;
+			// Light 2
+			R2L2.color = Color.red; 
+
+			//Room 4
+			//Light 1
+			R4L1.intensity = 0.1f;
+			R4L1.color = Color.red;	
+			// Light 2
+			R4L2.intensity = 1.0f;
+			R4L2.color = Color.blue;
+
+			//Room 5
+			//Light 1
+			R5L1.intensity = 0.1f;
+			R5L1.color = Color.red;	
+			// Light 2
+			R5L2.intensity = 1.0f;
+			R5L2.color = Color.blue;
+
+			//Room 3
+			// Light 1
+			R3L1.color = Color.blue;
+			// Light 3
+			R3L2.color = Color.red;
+
+			//Room 6
+			//Light 1
+			R6L1.intensity = 0.1f;
+			R6L1.color = Color.red;	
+			// Light 2
+			R6L2.intensity = 1.0f;
+			R6L2.color = Color.blue;
+
+			//Room 7
+			//Light 1
+			R7L1.intensity = 0.1f;
+			R7L1.color = Color.red;	
+			// Light 2
+			R7L2.intensity = 1.0f;
+			R7L2.color = Color.blue;
+			
+		} else {
+			// Do nothing and leave the lights White.
+		}
+			
+	}// End of Start function
+
+	/*
+	void LightSet(Light[] light, LightConfiguration config)
+	{
+		if (config == LightConfiguration.ONE) {
+			light [0].color = Color.blue;
+		}
+		if (config == LightConfiguration.TWO) {
+
+		}
+		if (config == LightConfiguration.THREE) {
+
+		}
+		if (config == LightConfiguration.FOUR) {
+
+		}
+		if (config == LightConfiguration.FIVE) {
+
+		}
+		if (config == LightConfiguration.SIX) {
+
+		}
+
+	}
+	*/
+}
---
+++ VR Scene Test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	/*
+    Simple flycam
+    Controls:
+    WASD  : Directional movement
+    Shift : Increase speed
+    Space : Moves camera directly up per its local Y-axis
+	*/
+
+	public float movementSpeed = 2.0f;   // Regular speed
+	public float shiftAdd  = 50.0f;   // Amount to accelerate when shift is pressed
+	public float maxShift  = 100.0f;  // Maximum speed when holding shift
+	public float camSens   = 0.15f;   // Mouse sensitivity
+
+	private Vector3 lastMouse = new Vector3(255, 255, 255);
+
+	void Update()
+	{
+		lastMouse = Input.mousePosition - lastMouse;
+		lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
+		lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
+		transform.eulerAngles = lastMouse;
+		lastMouse = Input.mousePosition;
+		// Mouse camera angle done.  
+
+		// Keyboard commands
+		Vector3 playerPosition = GetInput();
+		playerPosition *= movementSpeed;
+		playerPosition *= Time.deltaTime;
+		transform.Translate(playerPosition);
+	}
+
+	// Returns the basic values, if it's 0 than it's not active.
+	private Vector3 GetInput()
+	{
+		Vector3 playerMovement = new Vector3();
+
+		// Forwards
+		if (Input.GetKey(KeyCode.W))
+			playerMovement += new Vector3(0, 0, 1);
+
+		// Backwards
+		if (Input.GetKey(KeyCode.S))
+			playerMovement += new Vector3(0, 0, -1);
+
+		// Left
+		if (Input.GetKey(KeyCode.A))
+			playerMovement += new Vector3(-1, 0, 0);
+
+		// Right
+		if (Input.GetKey(KeyCode.D))
+			playerMovement += new Vector3(1, 0, 0);
+
+		// Up
+		if (Input.GetKey(KeyCode.Space))
+			playerMovement += new Vector3(0, 1, 0);
+
+		// Down
+		if (Input.GetKey(KeyCode.LeftControl))
+			playerMovement += new Vector3(0, -1, 0);
+
+		return playerMovement;
+	}
+}
