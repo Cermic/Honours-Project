f5029b7d2b37d4c7075c05bde41c454a6bc874a4
Jack Smith <streamlined50@hotmail.com>
2018-10-15 23:51:25 +0100

    Added room generation via script
    The Rooms can now be generated using script. However the emmisive material is not applying properly and the array structure is proving a problem for the room instantiation as it initialises as null values and this causes problems within the setup loop.

---
+++ VR Scene Test/Assets/Scripts/RoomLoad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomLoad : MonoBehaviour {
+
+	private GameObject roomRight;
+
+	// Use this for initialization
+	void Start () {
+		roomRight = Instantiate(Resources.Load("Prefabs/Room_Right"), new Vector3(0,30,0), Quaternion.identity) as GameObject;
+		Light light1 = roomRight.GetComponentInChildren (typeof(Light)) as Light;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+     
